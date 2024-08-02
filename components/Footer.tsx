08cd59ca70562f44add61f5f62fd9c9b6b05e107
timishot <timidotcom58@gmail.com>
2024-08-03 00:24:26 +0100

    fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className='w-full pb-10 mb-[100px] md:mb-5' id='contact'>
         <div className='flex flex-col items-center '>
             <h1 className='heading lg:max-w-[45vw]'>Ready to take <span className='text-purple'>Your</span> digital prensence to the next level </h1>
-            <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let's discuss how I can help you achieve your goals.</p>
+            <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve your goals.</p>
             <a href='/'>
                 <MagicButton title="Let's get in touch" icon={<FaLocationArrow />} position='right'/>
             </a>
