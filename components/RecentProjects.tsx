dd9c8a1cb4b20f70b5df81b8fd312dcfa3060a9d
timishot <timidotcom58@gmail.com>
2024-08-03 00:09:12 +0100

    push

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,45 +1,63 @@
-import { projects } from '@/data'
-import React from 'react'
-import { PinContainer } from './ui/3d-pin' //Error: Hydration failed because the initial UI does not match what was rendered on the server
+'use client'
+
+import { projects } from '@/data';
+import React, { useEffect, useState } from 'react';
+import { PinContainer } from './ui/3d-pin';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const RecentProjects = () => {
-  return (
-    <div className='py-20'>
-        <h1 className='heading'>
-            A small selection of {' '}
-            <span className='text-purple'>recent projects</span>
-        </h1>
-        <div className='flex flex-wrap items-center justify-center p-4 gap-16 mt-10'>
-            {projects.map(({ id , title, des,img, iconLists, link }) => (
-                <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:2096 w-[80vw]'>
-                    <PinContainer title={title} href={link} >
-                        <div className='relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10 '>
-                            <div className='relative w-full h-full  overflow-hidden lg:rounded-3xl bg-[#13162d]'>
-                                <img src='/bg.png' alt='bg-img'/>
-                            </div>
-                            <img src={img} alt='title' className='z-10 absolute bottom-0' />
-                        </div>
-                        <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
-                            {title}
-                        </h1>
-                        <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2  '>
-                            {des}
-                        </p>
-                        <div className='flex items-center justify-between mt-7 mb-3'>
-                            <div className='flex items-center'>
-                                {iconLists.map((icon, index) => (
-                                    <div key={icon} className='border border-white-100/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center' style={{transform: `translateX(-${5 * index * 2}px)`}}>
-                                        <img src={icon} alt={icon} className='p-2' />
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    </PinContainer>
-                </div>
-            ))}
-        </div>
-    </div>
-  )
-}
+  const [isClient, setIsClient] = useState(false);
 
-export default RecentProjects
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // Or some placeholder content to avoid hydration mismatch
+  }
+
+  return (
+    <div className='py-20' id='projects'>
+      <h1 className='heading'>
+        A small selection of {' '}
+        <span className='text-purple'>recent projects</span>
+      </h1>
+
+      <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
+          <div key={id} className='sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
+            <PinContainer title={link} href={link}>
+              <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10 '>
+                <div className='relative w-full h-full  overflow-hidden lg:rounded-3xl bg-[#13162d]'>
+                  <img src='/bg.png' alt='bg-img' />
+                </div>
+                <img src={img} alt={title} className='z-10 absolute bottom-0' />
+              </div>
+              <h1 className='font-bold lg:text-2xl md:text-xl text-base line-clamp-1'>
+                {title}
+              </h1>
+              <p className='lg:text-xl lg:font-normal font-light text-sm line-clamp-2'>
+                {des}
+              </p>
+              <div className='flex items-center justify-between mt-7 mb-3'>
+                <div className='flex items-center'>
+                  {iconLists.map((icon, index) => (
+                    <div key={icon} className='border border-white-100/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center' style={{ transform: `translateX(-${5 * index * 2}px)` }}>
+                      <img src={icon} alt={icon} className='p-2' />
+                    </div>
+                  ))}
+                </div>
+                <div className='flex items-center justify-center'>
+                  <p className='flex lg:text-xl md:text-wrapxs text-purple'>Check Live Site</p>
+                  <FaLocationArrow className='ms-3' color='#CDAC79' />
+                </div>
+              </div>
+            </PinContainer>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecentProjects;
