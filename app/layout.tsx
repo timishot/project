da28036674bfb2423767fa2dfee0ff3bf4713b5a
Timilehin Adeniran <101478000+timishot@users.noreply.github.com>
2025-01-31 10:01:44 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Timishot Portfolio",
-  description: "Mordern & Minimalist JS Mastery Portfolio "  ,
+  description: "Mordern & Minimalist Portfolio"  ,
 };
 
 export default function RootLayout({
